3a1ccebca9d68c126673d422dcfbaef636105fff
Se1roon <se1roonm@gmail.com>
2022-08-16 11:33:16 +0200

    Modify render method of Game component and add styling of it.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Versions from "./Versions";
 
 const Game = () => {
   const [isPlaying, setPlaying] = useState(false);
@@ -6,7 +7,12 @@ const Game = () => {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
-  return <main className="game"></main>;
+  return (
+    <main className="game">
+      <h2>Select game version you want to play!</h2>
+      <Versions />
+    </main>
+  );
 };
 
 export default Game;
