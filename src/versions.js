408979c12078088e9c746f54a4a3f69900af2e50
Se1roon <se1roonm@gmail.com>
2022-08-16 11:31:38 +0200

    Add versions data.

---
+++ src/versions.js
@@ -0,0 +1,33 @@
+const versions = [
+  {
+    name: "Gotei 13 Capitans",
+    numberOfCards: 13,
+    cards: [],
+  },
+  {
+    name: "Espada",
+    numberOfCards: 9,
+    cards: [],
+  },
+  {
+    name: "Fullbringers and Quincy",
+    numberOfCards: 14,
+    cards: [],
+  },
+  {
+    name: "15 Male Characters",
+    numberOfCards: 15,
+    cards: [],
+  },
+  {
+    name: "15 Female Characters",
+    numberOfCards: 15,
+    cards: [],
+  },
+];
+
+function getVersions() {
+  return versions;
+}
+
+export { getVersions };
