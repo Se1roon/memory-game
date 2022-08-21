e07b87c2323a08f5ee44c36e6c39e2d454ac3cc6
Se1roon <se1roonm@gmail.com>
2022-08-21 11:36:45 +0200

    Remove unnecessary lines.

--- src/shuffleItems.js
+++ src/shuffleItems.js
@@ -1,7 +1,4 @@
 function changeIndexes(array) {
-  // This function modifies the original array,
-  //  so it have to be used on copy of the state version.cards.
-
   const randomIndex = Math.round((Math.random() * 100) % array.length) || 2;
   const deletedItems = array.splice(0, randomIndex);
   const middle = Math.floor(array.length / 2);
